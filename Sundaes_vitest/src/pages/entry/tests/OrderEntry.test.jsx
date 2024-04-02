a48611a8499384fce6c59dc92cbdc4ba36fbf702
EllaLiu0401 <ellatong0515@gmail.com>
2024-04-02 23:47:31 +1100

    Add sundae server

---
+++ Sundaes_vitest/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,4 @@
+import { render, screen } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+
+test("handles error for scopps and toppings routes", () => {});
--- Sundaes_vitest/src/setupTests.js
+++ Sundaes_vitest/src/setupTests.js
@@ -4,9 +4,15 @@ import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "../src/mocks/server";
 
 // Establish API mocking before all tests
-beforeAll(() => server.listen());
+beforeAll(() => {
+  server.listen();
+});
 // Reset anh request handlers that we may add during the tests,
 // so they don't affect other tests
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+});
 // Clean up after the test are finished
-afterAll(() => server.close());
+afterAll(() => {
+  server.close();
+});
