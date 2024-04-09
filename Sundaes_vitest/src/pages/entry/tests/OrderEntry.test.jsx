652bece3f4d46dfda80e5d07a3e6475ac855b6b3
EllaLiu0401 <ellatong0515@gmail.com>
2024-04-09 17:56:40 +1000

    Feat: Add error server reaponse check. - Implemented mock service worker and handlers. - Set error test. - Implemented react code. - Updated the msw version.
    Involved files:
    - .DS_Store
    - Sundaes_vitest/package-lock.json
    - Sundaes_vitest/package.json
    - Sundaes_vitest/src/pages/common/AlertBanner.jsx
    - Sundaes_vitest/src/pages/entry/Options.jsx
    - Sundaes_vitest/src/pages/entry/OrderEntry.jsx
    - Sundaes_vitest/src/pages/entry/tests/OrderEntry.test.jsx

--- Sundaes_vitest/src/pages/entry/tests/OrderEntry.test.jsx
+++ Sundaes_vitest/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,4 +1,22 @@
+import { server } from "../../../mocks/server";
 import { render, screen } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
+import { http, HttpResponse } from "msw";
 
-test("handles error for scopps and toppings routes", () => {});
+test("handles error for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    http.get("http://localhost:3030/scoops", () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+    http.get("http://localhost:3030/toppings", () => {
+      return new HttpResponse(null, { status: 500 });
+    })
+  );
+
+  render(<OrderEntry />);
+
+  const alerts = await screen.findAllByText(
+    "An unexpected error occurred. Please try again later"
+  );
+  expect(alerts).toHaveLength(2);
+});
