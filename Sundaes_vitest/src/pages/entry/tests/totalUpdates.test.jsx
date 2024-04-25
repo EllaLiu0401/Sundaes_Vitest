3e60cd1c76f2a5c68713cf5de2f491c9e4cf2221
EllaLiu0401 <ellatong0515@gmail.com>
2024-04-25 17:50:23 +1000

    Feat: Add Topping subtotal check
    - Added topping check like  scoop expect checkbox.
    - Added related react code.
    
    Involved files:
    - Sundaes_vitest/src/pages/entry/ToppingOption.jsx
    - Sundaes_vitest/src/pages/entry/tests/totalUpdates.test.jsx

--- Sundaes_vitest/src/pages/entry/tests/totalUpdates.test.jsx
+++ Sundaes_vitest/src/pages/entry/tests/totalUpdates.test.jsx
@@ -2,10 +2,11 @@ import { render, screen } from "../../../test-utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 import { OrderDetailsProvider } from "../../../context/OrderDetails";
+import { expect } from "vitest";
 
 test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-  render(<Options optionType="scoops" />);
+  render(<Options optionType={"scoops"} />);
 
   // make sure total starts out at $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false }); // exact: false: partial match
@@ -26,3 +27,31 @@ test("update scoop subtotal when scoops change", async () => {
   await user.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update topping subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType={"toppings"} />);
+
+  // make sure total starts out at $0.00
+  const toppingSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+
+  expect(toppingSubtotal).toHaveTextContent("0.00");
+  // Tick Hot fudge topping, and check subtotal
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingSubtotal).toHaveTextContent("1.50");
+  // Tick Cheery topping, and check subtotal
+  const cherryCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherryCheckbox);
+  expect(toppingSubtotal).toHaveTextContent("3.00");
+  // Tick Hot fudge tooping off, and check subtotal
+  await user.click(hotFudgeCheckbox);
+  expect(toppingSubtotal).toHaveTextContent("1.50");
+});
+1;
