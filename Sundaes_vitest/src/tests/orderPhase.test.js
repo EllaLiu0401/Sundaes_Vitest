5aae44a88511a833284528fd677e25edd6a4c14d
EllaLiu0401 <ellatong0515@gmail.com>
2024-06-11 16:14:45 +1000

    Feat: Add OrderPhase testing.
    - Order phases for happy path
    - Check procedure:
    	- Check create order.
    	- Check Accept terms and submit.
    	- Check order summary.
    	- Check new order reset.
    
    Involved files:
    - Sundaes_vitest/src/tests/orderPhase.test.js
    - Sundaes_vitest/src/mocks/handlers.js
    - Sundaes_vitest/src/mocks/server.js

---
+++ Sundaes_vitest/src/tests/orderPhase.test.js
@@ -0,0 +1,85 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+import { expect } from "vitest";
+
+test("order phases for happy path", async () => {
+  // render app
+  const user = userEvent.setup();
+  const { unmount } = render(<App />);
+
+  // add ice cream scoops and toppings
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  await user.click(hotFudgeCheckbox);
+
+  // find and click order button
+  const orderButton = await screen.findByRole("button", {
+    name: "Order Sundaes",
+  });
+  await user.click(orderButton);
+
+  // check summary information based on order
+  const summaryScoopsHeading = await screen.findByRole("heading", {
+    name: /Scoops:/i,
+  });
+  expect(summaryScoopsHeading).toHaveTextContent("2.00");
+  const summaryToppingsHeading = await screen.findByRole("heading", {
+    name: "Toppings: $1.50",
+  });
+  expect(summaryToppingsHeading).toBeIntheDocument();
+
+  // expect(screen.getByText("1 Vanilla")).toBeIntheDocument();
+  // expect(screen.getByText("Hot fudge")).toBeIntheDocument();
+  const optionItems = screen.getAllByRole("listitem");
+  const optionItemsText = optionItems.map((item) => item.textContent);
+  expect(optionItemsText).toEqual(["1 Vanilla", "Hot fudge"]);
+
+  const summaryHeading = await screen.findByRole("heading", { name: /Total/i });
+  expect(summaryHeading).toHaveTextContent("3.50");
+
+  // accpet terms and conditions and click button to confirm order
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const buttonElement = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  await user.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  await user.click(buttonElement);
+
+  // expect 'loading' to show
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeIntheDocument();
+  // confirm order number on confirmation page
+  const thankYouHeader = await screen.findByRole("heading", {
+    name: /Thank you/i,
+  });
+  expect(thankYouHeader).toBeIntheDocument();
+  const notLoading = screen.queryByText("loading");
+  expect(notLoading).not.toBeIntheDocument();
+  const orderNumber = await screen.findByRole("heading", {
+    name: /order number/i,
+  });
+  expect(orderNumber).toBeIntheDocument();
+  // click 'new order' button on confirmation page
+  const newOrderButton = await screen.findByRole("button", {
+    name: /Create new order/i,
+  });
+  await user.click(newOrderButton);
+  // check that scoops and toppings have been reset
+  const scoopsTotal = await screen.findByText("Scoops total: $0.00");
+  expect(scoopsTotal).toBeIntheDocument();
+  const toppingsTotal = screen.getByText("Toppings total: $0.00");
+  expect(toppingsTotal).toBeIntheDocument();
+
+  // unmount the component to trigger cleanup and avoid
+  unmount();
+});
